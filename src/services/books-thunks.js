a6745b7cdd563a11d0672e36895f63296d4e3da0
Harshit Gajjar <hrstgajjar3@gmail.com>
2023-04-23 21:40:43 -0700

    resolved merge conflicts

--- src/services/books-thunks.js
+++ src/services/books-thunks.js
@@ -1,33 +1,37 @@
-import axios from 'axios'
-import { createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const BOOKS_API = 'http://localhost:8081/book';
+const BOOKS_API = "http://localhost:8081/book";
 
-export const getAllBooksThunk = createAsyncThunk('books/all',
-    async () => {
-        const response = await axios.get(`${BOOKS_API}/`);
-        return response.data;
-    })
+export const getAllBooksThunk = createAsyncThunk("books/all", async () => {
+  const response = await axios.get(`${BOOKS_API}`);
+  return response.data;
+});
 
-export const getBookByIsbnThunk = createAsyncThunk('books/isbn',
-    async (isbn, { rejectWithValue }) => {
-        try {
-            const response = await axios.get(`${BOOKS_API}/${isbn}`);
-            return response.data;
-        } catch (err) {
-            return rejectWithValue(err.response.data);
-        }
-    })
+export const getBookByIsbnThunk = createAsyncThunk(
+  "books/isbn",
+  async (isbn, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${BOOKS_API}/${isbn}`);
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
 
+export const createBookThunk = createAsyncThunk(
+  "books/create",
+  async (book) => {
+    const response = await axios.post(BOOKS_API, book);
+    return response.data;
+  }
+);
 
-export const createBookThunk = createAsyncThunk('books/create',
-    async (book) => {
-        const response = await axios.post(BOOKS_API, book);
-        return response.data;
-    })
-
-export const updateBookInventoryByIsbnThunk = createAsyncThunk('books/updateInventory',
-    async ({isbn, inventory}) => {
-        const response = await axios.put(`${BOOKS_API}/${isbn}`, inventory);
-        return response.data;
-    })
+export const updateBookInventoryByIsbnThunk = createAsyncThunk(
+  "books/updateInventory",
+  async ({ isbn, inventory }) => {
+    const response = await axios.put(`${BOOKS_API}/${isbn}`, inventory);
+    return response.data;
+  }
+);
